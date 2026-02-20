0cc5d707c90c682fb1e13fa1c7e3f2aca3def9e2
Ryan <ryandasilva233@gmail.com>
2026-02-20 17:35:44 -0300

    Iniciando

--- mercado/__init__.py
+++ mercado/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
+app.config["SECRET_KEY"] = '9277215997e456caf1b82675'
 db.init_app(app)
 
 from mercado import routes
--- mercado/routes.py
+++ mercado/routes.py
@@ -1,6 +1,7 @@
 from mercado import app
 from flask import render_template
 from mercado.models import Item
+from mercado.forms import CadastroForm
 
 @app.route('/')
 def page_home():
@@ -9,6 +10,9 @@ def page_home():
 @app.route('/produtos')
 def page_produto():
     itens=Item.query.all()
-
     return render_template("produtos.html",itens=itens)
 
+@app.route('/cadastro')
+def page_cadasteo():
+    form = CadastroForm()
+    return render_template("Cadastro.html", form=form)
