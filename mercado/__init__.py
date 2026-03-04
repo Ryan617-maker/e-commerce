03ce3c915fae26ac9abcd4ee9bbafe2f33eada49
Ryan <ryandasilva233@gmail.com>
2026-03-04 17:28:21 -0300

    parte de login

--- mercado/__init__.py
+++ mercado/__init__.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
-db = SQLAlchemy()
+db = SQLAlchemy ()
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///mercado.db"
-app.config["SECRET_KEY"] = '84c97cbcb7409c1f7e2f8015'
+app.config[ 'SQLALCHEMY_DATABASE_URI'] = "sqlite:///mercado.db"
+app.config["SECRET_KEY"] = 'd6d4271ff499e54dd0e6b673'
 db.init_app(app)
+bcrypt=Bcrypt(app)
 
 from mercado import routes
--- mercado/routes.py
+++ mercado/routes.py
@@ -1,7 +1,8 @@
+
 from mercado import app
-from flask import render_template,redirect,url_for, flash
+from flask import render_template,redirect,url_for,flash
 from mercado.models import Item,User
-from mercado.forms import CadastroForm
+from mercado.forms import CadastroForm, LoginForm
 from mercado import db
 
 @app.route('/')
@@ -20,14 +21,18 @@ def page_cadastro():
         usuario = User(
             usuario = form.usuario.data,
             email = form.email.data,
-            senha = form.senha1.data
+            senhacrip = form.senha1.data
         )
 
         db.session.add(usuario)
         db.session.commit()
         return redirect(url_for('page_produto'))
-    
     if form.errors != {}:
         for err in form.errors.values():
-            flash(f'Erro ao cadatrar usuário {err}', category="danger")
+            flash(f'Erro ao cadastrar usuário {err} ',category="danger")
     return render_template("cadastro.html", form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def page_login():
+    form =LoginForm()
+    return render_template('login.html',form=form)
